e1851c45c57a469f0c73744eb41172abfceb9406
Prathibhaponnu <100864129+Prathibhaponnu@users.noreply.github.com>
2022-05-17 16:20:07 +0530

    Add files via upload

---
+++ swap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+num1=10
+num2=20
+echo "before swapping"
+echo  "num1:$num1"
+echo "num2:$num2"
+num3=$num1
+num1=$num2
+num2=$num3
+echo "after swapping"
+echo "num1:$num1"
+echo "num2:$num2"
+
